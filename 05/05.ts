19df7653662a928916b4adf9939c992d51184dc2
Evokoo <evanjohncooper@gmail.com>
2025-01-07 01:40:22 +1100

    Day 05 - Part 1 complete

--- 05/05.ts
+++ 05/05.ts
@@ -1,10 +1,11 @@
 // Imports
 import TOOLS from "tools";
+import * as CRYPTO from "node:crypto";
 
 //Solutions
-export function solveA(fileName: string, day: string): number {
+export function solveA(fileName: string, day: string): string {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return getPassword(data);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -12,3 +13,16 @@ export function solveB(fileName: string, day: string): number {
 }
 
 // Functions
+function getPassword(input: string) {
+	let password = "";
+
+	for (let i = 0; password.length < 8; i++) {
+		const hash = CRYPTO.createHash("md5").update(`${input}${i}`).digest("hex");
+
+		if (hash.startsWith("00000")) {
+			password += hash.at(5);
+		}
+	}
+
+	return password;
+}
