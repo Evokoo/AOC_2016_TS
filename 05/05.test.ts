059c877245bf039aad1bbd77e4ffc1c10b379a21
Evokoo <evanjohncooper@gmail.com>
2025-01-07 02:08:19 +1100

    Day 05 complete

--- 05/05.test.ts
+++ 05/05.test.ts
@@ -16,13 +16,13 @@ describe(`AOC 2016 - Day ${currentDay}`, () => {
 		});
 	});
 
-	describe.skip("Part B", () => {
+	describe("Part B", () => {
 		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
+			expect(solveB("example_b", currentDay)).toBe("05ace8e3");
 		});
 
-		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveB("input", currentDay)).toBe("863dde27");
 		});
 	});
 });
--- 05/05.ts
+++ 05/05.ts
@@ -7,22 +7,32 @@ export function solveA(fileName: string, day: string): string {
 	const data = TOOLS.readData(fileName, day);
 	return getPassword(data);
 }
-export function solveB(fileName: string, day: string): number {
+export function solveB(fileName: string, day: string): string {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return getPassword(data, true);
 }
 
 // Functions
-function getPassword(input: string) {
-	let password = "";
+function getPassword(input: string, indexed: boolean = false) {
+	const characterMap: Map<number, string> = new Map();
 
-	for (let i = 0; password.length < 8; i++) {
+	for (let i = 0; characterMap.size < 8; i++) {
 		const hash = CRYPTO.createHash("md5").update(`${input}${i}`).digest("hex");
 
 		if (hash.startsWith("00000")) {
-			password += hash.at(5);
+			const [a, b] = hash.slice(5, 7);
+
+			if (indexed) {
+				const index = parseInt(a, 16);
+
+				if (index < 8 && !characterMap.has(index)) {
+					characterMap.set(index, b);
+				}
+			} else {
+				characterMap.set(characterMap.size, a);
+			}
 		}
 	}
 
-	return password;
+	return Array.from({ length: 8 }, (_, i) => characterMap.get(i)).join("");
 }
