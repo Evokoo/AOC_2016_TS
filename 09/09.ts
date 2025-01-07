15509665fb6de5f31ec039ec187a255f528e5c8d
Evokoo <evanjohncooper@gmail.com>
2025-01-07 19:03:35 +1100

    Day 09 complete

--- 09/09.ts
+++ 09/09.ts
@@ -8,26 +8,30 @@ export function solveA(fileName: string, day: string): number {
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return decompressInput(data, true);
 }
 
 // Functions
-function decompressInput(data: string) {
-	let count = 0;
+function decompressInput(data: string, nested: boolean = false) {
+	function decompress(data: string, count: number = 0): number {
+		if (!data) {
+			return count;
+		} else if (data[0] === "(") {
+			const formula = (data.match(/\([\dx]+\)/) || [""])[0];
+			const [chars, repeat] = (formula.match(/\d+/g) || []).map(Number);
+			const dataSlice = data.slice(formula.length, chars + formula.length);
+			const newData = data.slice(chars + formula.length);
 
-	while (data.length) {
-		if (data[0] === "(") {
-			const decompression = (data.match(/\([\dx]+\)/) || [""])[0];
-			const [chars, repeat] = (decompression.match(/\d+/g) || []).map(Number);
-
-			count += chars * repeat;
-			data = data.slice(chars + decompression.length);
+			if (nested) {
+				return decompress(newData, count + decompress(dataSlice) * repeat);
+			} else {
+				return decompress(newData, count + chars * repeat);
+			}
 		} else {
 			const removed = (data.match(/[A-Z]+(?=\(|\b)/) || [""])[0];
-			count += removed.length;
-			data = data.slice(removed.length);
+			return decompress(data.slice(removed.length), count + removed.length);
 		}
 	}
 
-	return count;
+	return decompress(data);
 }
