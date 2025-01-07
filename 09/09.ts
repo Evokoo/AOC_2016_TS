571ea6370448560f0e4327059b0ca36ceb87e049
Evokoo <evanjohncooper@gmail.com>
2025-01-07 18:46:34 +1100

    Day 09 - Part 1 complete

--- 09/09.ts
+++ 09/09.ts
@@ -4,7 +4,7 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return decompressInput(data);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -12,3 +12,22 @@ export function solveB(fileName: string, day: string): number {
 }
 
 // Functions
+function decompressInput(data: string) {
+	let count = 0;
+
+	while (data.length) {
+		if (data[0] === "(") {
+			const decompression = (data.match(/\([\dx]+\)/) || [""])[0];
+			const [chars, repeat] = (decompression.match(/\d+/g) || []).map(Number);
+
+			count += chars * repeat;
+			data = data.slice(chars + decompression.length);
+		} else {
+			const removed = (data.match(/[A-Z]+(?=\(|\b)/) || [""])[0];
+			count += removed.length;
+			data = data.slice(removed.length);
+		}
+	}
+
+	return count;
+}
