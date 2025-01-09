7f95b7486d9787ddaa582b536465ee853a0e231a
Evokoo <evanjohncooper@gmail.com>
2025-01-10 01:33:05 +1100

    Day 13 complete

--- 13/13.test.ts
+++ 13/13.test.ts
@@ -8,21 +8,21 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2016 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(11);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(86);
 		});
 	});
 
-	describe.skip("Part B", () => {
-		it("Example", () => {
+	describe("Part B", () => {
+		it.skip("Example", () => {
 			expect(solveB("example_b", currentDay)).toBe(0);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveB("input", currentDay)).toBe(127);
 		});
 	});
 });
--- 13/13.ts
+++ 13/13.ts
@@ -4,11 +4,71 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const [target, fNumber] = fileName.startsWith("example")
+		? [{ x: 7, y: 4 }, 10]
+		: [{ x: 31, y: 39 }, Number(data)];
+	return traverseMap(target, fNumber);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return traverseMap({ x: Infinity, y: Infinity }, Number(data), 50);
 }
 
+type Point = { x: number; y: number };
+type State = Point & { steps: number };
+
 // Functions
+function traverseMap(target: Point, fNumber: number, range?: number): number {
+	const queue: State[] = [{ x: 1, y: 1, steps: 0 }];
+	const tiles: Map<string, string> = new Map();
+	const seen: Set<string> = new Set();
+
+	while (queue.length) {
+		const current = queue.shift()!;
+
+		if (range && current.steps === range) {
+			return seen.size;
+		} else if (current.x === target.x && current.y === target.y) {
+			return current.steps;
+		} else {
+			pushNewStates(current);
+		}
+	}
+
+	throw Error("Path to target not found");
+
+	function pushNewStates({ x, y, steps }: State) {
+		const directions = [
+			[0, 1],
+			[0, -1],
+			[1, 0],
+			[-1, 0],
+		];
+
+		for (const [dx, dy] of directions) {
+			const [nx, ny] = [dx + x, dy + y];
+			const coord = `${nx},${ny}`;
+
+			if (nx < 0 || ny < 0) continue;
+
+			if (getTile(nx, ny, coord) === "." && !seen.has(coord)) {
+				queue.push({ x: nx, y: ny, steps: steps + 1 });
+				seen.add(coord);
+			}
+		}
+	}
+
+	function getTile(x: number, y: number, coord: string): string {
+		if (tiles.has(coord)) {
+			return tiles.get(coord)!;
+		} else {
+			const tileValue = Math.pow(x + y, 2) + 3 * x + y + fNumber;
+			const bits = tileValue.toString(2).replace(/0/g, "");
+			const tile = bits.length % 2 === 0 ? "." : "#";
+
+			tiles.set(coord, tile);
+
+			return tile;
+		}
+	}
+}
