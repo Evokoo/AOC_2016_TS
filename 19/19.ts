fb9ebe689792f2098fdb389f08326ef7cd142bcf
Evokoo <evanjohncooper@gmail.com>
2025-01-11 01:29:34 +1100

    Day 19 complete

--- 19/19.ts
+++ 19/19.ts
@@ -4,11 +4,34 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return josephusFormula(+data, 2);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return findLastElf(+data);
 }
 
-// Functions
+//Functions
+function josephusFormula(n: number, k: number): number {
+	if (n === 1) return 1;
+
+	let position = 0;
+	for (let i = 2; i <= n; i++) {
+		position = (position + k) % i;
+	}
+
+	return position + 1;
+}
+function findLastElf(n: number) {
+	let elf = 1;
+
+	for (let i = 1; i < n; i++) {
+		elf = (elf % i) + 1;
+
+		if (elf > (i + 1) / 2) {
+			elf++;
+		}
+	}
+
+	return elf;
+}
