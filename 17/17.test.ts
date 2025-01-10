0d04a08a7c9b04292dcc71e9ea31f27a27819121
Evokoo <evanjohncooper@gmail.com>
2025-01-10 18:25:38 +1100

    Day 17 complete

--- 17/17.test.ts
+++ 17/17.test.ts
@@ -8,21 +8,23 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2016 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(
+				"DRURDRUDDLLDLUURRDULRLDUUDDDRR"
+			);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe("RDRLDRDURD");
 		});
 	});
 
-	describe.skip("Part B", () => {
+	describe("Part B", () => {
 		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
+			expect(solveB("example_b", currentDay)).toBe(370);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveB("input", currentDay)).toBe(596);
 		});
 	});
 });
--- 17/17.ts
+++ 17/17.ts
@@ -1,14 +1,72 @@
 // Imports
 import TOOLS from "tools";
+import * as CRYPTO from "node:crypto";
 
 //Solutions
-export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+export function solveA(fileName: string, day: string): string {
+	const passcode = TOOLS.readData(fileName, day);
+	return traverseValut(passcode).path;
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const passcode = TOOLS.readData(fileName, day);
+	return traverseValut(passcode, true).path.length;
 }
 
+type State = { x: number; y: number; path: string };
+type Direction = { dx: number; dy: number; code: string };
+
 // Functions
+function traverseValut(passcode: string, longest: boolean = false): State {
+	const queue: State[] = [{ x: 0, y: 0, path: "" }];
+	const longRoute: State = { x: 0, y: 0, path: "" };
+
+	while (queue.length) {
+		const current = queue.shift()!;
+
+		if (current.x === 3 && current.y === 3) {
+			if (longest) {
+				if (current.path.length > longRoute.path.length) {
+					longRoute.x = current.x;
+					longRoute.y = current.y;
+					longRoute.path = current.path;
+				}
+			} else {
+				return current;
+			}
+		} else {
+			pushNewStates(current);
+		}
+	}
+
+	if (longest) {
+		return longRoute;
+	} else {
+		throw Error("Path not found");
+	}
+
+	function pushNewStates({ x, y, path }: State): void {
+		for (const { dx, dy, code } of getOpenDoors(passcode, path)) {
+			const [nx, ny] = [dx + x, dy + y];
+
+			if (nx >= 0 && nx < 4 && ny >= 0 && ny < 4) {
+				queue.push({ x: nx, y: ny, path: path + code });
+			}
+		}
+	}
+	function getOpenDoors(passcode: string, path: string): Direction[] {
+		const directions = [
+			{ dx: 0, dy: -1, code: "U" },
+			{ dx: 0, dy: 1, code: "D" },
+			{ dx: -1, dy: 0, code: "L" },
+			{ dx: 1, dy: 0, code: "R" },
+		];
+
+		const hash = CRYPTO.createHash("md5")
+			.update(`${passcode}${path}`)
+			.digest("hex");
+
+		return directions.filter((_, i) => {
+			return hash[i] >= "b" && hash[i] <= "f";
+		});
+	}
+}
