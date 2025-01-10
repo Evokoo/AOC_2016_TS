9de51cf6b27c9cd679b0238b31eaf7e6cb4d02d8
Evokoo <evanjohncooper@gmail.com>
2025-01-11 01:18:17 +1100

    Day 18 complete

--- 18/18.test.ts
+++ 18/18.test.ts
@@ -8,21 +8,21 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2016 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(38);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(1982);
 		});
 	});
 
-	describe.skip("Part B", () => {
+	describe("Part B", () => {
 		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
+			expect(solveB("example_b", currentDay)).toBe(1935478);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveB("input", currentDay)).toBe(20005203);
 		});
 	});
 });
--- 18/18.ts
+++ 18/18.ts
@@ -4,11 +4,38 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const limit = fileName.startsWith("example") ? 10 : 40;
+	return countSafeTiles(data, limit);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return countSafeTiles(data, 400000);
 }
 
 // Functions
+function countSafeTiles(row: string, limit: number): number {
+	let count = row.replace(/\^/g, "").length;
+
+	for (let i = 0; i < limit - 1; i++) {
+		let nextRow = "";
+
+		for (let j = 0; j < row.length; j++) {
+			const tile = getTile(row[j - 1] ?? ".", row[j], row[j + 1] ?? ".");
+			if (tile === ".") count++;
+			nextRow += tile;
+		}
+		row = nextRow;
+	}
+	return count;
+}
+
+function getTile(l: string, c: string, r: string): string {
+	const patterns: Record<string, string> = {
+		"^^.": "^",
+		".^^": "^",
+		"^..": "^",
+		"..^": "^",
+	};
+
+	return patterns[l + c + r] ?? ".";
+}
