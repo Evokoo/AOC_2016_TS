532480f47de0ac35fbe4bb87ca5af50ec3d87e2d
Evokoo <evanjohncooper@gmail.com>
2025-01-11 16:50:24 +1100

    Day 22 - Part 1 complete

--- 22/22.test.ts
+++ 22/22.test.ts
@@ -7,12 +7,12 @@ const currentDay = path.basename(Deno.cwd());
 
 describe(`AOC 2016 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
-		it("Example", () => {
+		it.skip("Example", () => {
 			expect(solveA("example_a", currentDay)).toBe(0);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(892);
 		});
 	});
 
--- 22/22.ts
+++ 22/22.ts
@@ -4,11 +4,51 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const nodes = parseInput(data);
+	return countNodePairs(nodes);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
 	return 0;
 }
 
+type Node = {
+	x: number;
+	y: number;
+	total: number;
+	used: number;
+	free: number;
+};
+
+type Nodes = Map<string, Node>;
+
 // Functions
+function parseInput(data: string): Nodes {
+	const fileList: Nodes = new Map();
+
+	for (const file of data.split("\n").slice(2)) {
+		const [x, y, total, used, free] = (file.match(/\d+/g) || []).map(Number);
+		fileList.set(`${x},${y}`, { x, y, total, used, free });
+	}
+
+	return fileList;
+}
+
+function countNodePairs(nodes: Nodes): number {
+	const allNodes = [...nodes];
+	let pairs = 0;
+
+	for (let i = 0; i < allNodes.length; i++) {
+		const a = allNodes[i];
+
+		for (let j = i + 1; j < allNodes.length; j++) {
+			const b = allNodes[j];
+
+			if (a[0] === b[0]) continue;
+			if (a[1].used > 0 && a[1].used <= b[1].free) pairs++;
+			if (b[1].used > 0 && b[1].used <= a[1].free) pairs++;
+		}
+	}
+
+	return pairs;
+}
